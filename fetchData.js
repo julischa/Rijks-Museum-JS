2eee031de032e0b2d4d9b8fb689feaf556109159
julescwrt <julianeschawert@gmail.com>
2023-03-20 13:22:02 +0100

    Updated style.css

--- fetchData.js
+++ fetchData.js
@@ -1,171 +1,81 @@
-let url = "https://www.rijksmuseum.nl/api/nl/collection?key=Hu8sPHp0&involvedMaker=Rembrandt+van+Rijn"
-let url2 = "https://www.rijksmuseum.nl/api/nl/collection?key=Hu8sPHp0&involvedMaker=Johannes+Vermeer"
+const url =
+  "https://www.rijksmuseum.nl/api/nl/collection?key=Hu8sPHp0&involvedMaker=Rembrandt+van+Rijn";
+const url2 =
+  "https://www.rijksmuseum.nl/api/nl/collection?key=Hu8sPHp0&involvedMaker=Johannes+Vermeer";
 
-
-function FetchRembrandData(artistName) {
-	let testUrl = `https://www.rijksmuseum.nl/api/nl/collection?key=Hu8sPHp0&involvedMaker=${artistName}`
-
-	console.log('artistName :>> ', artistName);
-	fetch(testUrl).then(
-		(response) => {
-
-			return response.json()
-		}
-	).then((result) => {
-		console.log('result :>> ', result);
-		render(result)
-	}).catch((error) => {
-		console.log('error :>> ', error);
-	})
+function fetchArtistData(artistName) {
+  const apiUrl = `https://www.rijksmuseum.nl/api/nl/collection?key=Hu8sPHp0&involvedMaker=${artistName}`;
+  console.log("artistName:", artistName);
+  fetch(apiUrl)
+    .then((response) => response.json())
+    .then((result) => {
+      console.log("result:", result);
+      render(result);
+    })
+    .catch((error) => {
+      console.log("error:", error);
+    });
 }
-
 
 function createButtons() {
-	/// TEST AREA
-	// create buttons for veermer and rembrandt
-	const buttonContainer = document.getElementById("button-container")
+  const buttonContainer = document.getElementById("button-container");
 
-	//create rembrandt button
-	let rembrandtButton = document.createElement("button")
-	rembrandtButton.setAttribute("id", "rembrandt")
-	rembrandtButton.setAttribute("class", "px-4 py-2 mt-2 mx-2")
-	rembrandtButton.innerText = "REMBRANDT"
-	rembrandtButton.value = "Rembrandt van Rijn"
-	buttonContainer.appendChild(rembrandtButton)
+  const createButton = (text, value) => {
+    const button = document.createElement("button");
+    button.setAttribute("class", "px-4 py-2 mt-2 mx-2 red-button");
+    button.innerText = text;
+    button.value = value;
+    buttonContainer.appendChild(button);
+    return button;
+  };
 
-	//create Vermeer button
-	let veermerButton = document.createElement("button")
-	veermerButton.setAttribute("id", "vermeer")
-	veermerButton.setAttribute("class", "px-4 py-2 mt-2 mx-2")
-	veermerButton.innerText = "VERMEER"
-	veermerButton.value = "Johannes Vermeer"
-	buttonContainer.appendChild(veermerButton)
-	
-	 // create delete button
-  let deleteButton = document.createElement("button");
+  const rembrandtButton = createButton("REMBRANDT", "Rembrandt van Rijn");
+  const vermeerButton = createButton("VERMEER", "Johannes Vermeer");
+  const deleteButton = createButton("✶");
   deleteButton.setAttribute("id", "delete-button");
-  deleteButton.setAttribute("class", "px-4 py-2 mt-2 mx-2")
-  deleteButton.innerText = "✶";
-  buttonContainer.appendChild(deleteButton);
 
-  addEvents();
+  rembrandtButton.classList.add("red-button");
+  vermeerButton.classList.add("red-button");
+  deleteButton.classList.add("red-button");
 
-rembrandtButton.classList.add('red-button');
-veermerButton.classList.add('red-button');
-deleteButton.classList.add('red-button');
-
-
+  addEvents(rembrandtButton, vermeerButton, deleteButton);
 }
 
+function addEvents(rembrandtButton, vermeerButton, deleteButton) {
+  rembrandtButton.addEventListener("click", (event) => {
+    const artistName = event.target.value;
+    fetchArtistData(artistName);
+  });
 
-function addEvents() {
-	const rembrandtButton = document.getElementById("rembrandt")
-	const veermerButton = document.getElementById("vermeer")
+  vermeerButton.addEventListener("click", (event) => {
+    fetchArtistData("Johannes Vermeer");
+  });
 
-	rembrandtButton.addEventListener("click", (event) => {
-		console.log('event.target :>> ', event.target.value);
-		const artistName = event.target.value
-		FetchRembrandData(artistName)
-
-	})
-	veermerButton.addEventListener("click", (event) => {
-		console.log("I've been clicked");
-		console.log('event.target :>> ', event.target.value);
-		FetchVermeerData()
-	})
-
-	const deleteButton = document.getElementById("delete-button");
-	deleteButton.addEventListener("click", () => {
-		const cardcontainerToDelete = document.getElementById("card-container");
-		cardcontainerToDelete.innerHTML = ""
-		//elementToDelete.parentNode.removeChild(elementToDelete);
-	});
-
-
+  deleteButton.addEventListener("click", () => {
+    const cardContainer = document.getElementById("card-container");
+    cardContainer.innerHTML = "";
+  });
 }
-
-
-createButtons()
-
 
 function render(api) {
-	console.log('result inside Render :>> ', api);
-	// get body element from DOM  
-	let cardContainer = document.getElementById("card-container");
-	cardContainer.innerHTML = ""
+  const cardContainer = document.getElementById("card-container");
+  cardContainer.innerHTML = "";
 
-	for (let i = 0; i < api.artObjects.length; i++) {
-		// create html element / tags 
+  for (let i = 0; i < api.artObjects.length; i++) {
+    const title = api.artObjects[i].title.split(" ").slice(0, 4).join(" ");
+    const img = document.createElement("img");
+    img.setAttribute("src", api.artObjects[i].webImage.url);
 
-		//h4 tag title
-		let title = document.createElement("h4")
-		title.innerText = api.artObjects[i].title
-		console.log(title.innerText);
+    const author = document.createElement("p");
+    author.innerText = api.artObjects[i].principalOrFirstMaker;
 
-		//shorten text to 4 words 
-		let fullText = api.artObjects[i].title;
-		let wordArray = fullText.split(" ");
-		let shortenedText = "";
+    const container = document.createElement("div");
+    container.appendChild(document.createElement("h4")).innerText = title;
+    container.appendChild(img);
+    container.appendChild(author);
 
-		if (wordArray.length > 4) {
-			// Shorten text to 5 words
-			for (let i = 0; i < 4; i++) {
-				shortenedText += wordArray[i] + " ";
-			}
-			shortenedText += "";
-		} else {
-			// Text is already less than 4 words, do not shorten
-			shortenedText = fullText;
-		}
-
-		title.innerText = shortenedText;
-
-
-		//img 
-		let img = document.createElement("img")
-		img.setAttribute("src", api.artObjects[i].webImage.url)
-		//img.setAttribute("width", 200)
-
-		//p tag  author
-		let author = document.createElement("p")
-		author.innerText = api.artObjects[i].principalOrFirstMaker
-
-		//div container
-		let container = document.createElement("div")
-
-
-		//appending title-img-author => container
-		container.appendChild(title)
-		container.appendChild(img)
-		container.appendChild(author)
-
-		//appending CONTAINER => body
-		cardContainer.appendChild(container)
-
-	}
-
-
+    cardContainer.appendChild(container);
+  }
 }
 
-// render()
-
-
-// vermeer function
-
-function FetchVermeerData() {
-	fetch(url2).then(
-		(response) => {
-
-			return response.json()
-		}
-	).then((result) => {
-		console.log('result :>> ', result);
-		render(result)
-	}).catch((error) => {
-		console.log('error :>> ', error);
-	})
-}
-
-// Define the API endpoint for searching the product catalog
-//var endpoint = "http://www.rijksmuseum.nl/api/nl/collection/SK-C-5";
-
+createButtons();
---
+++ main.js
@@ -0,0 +1,82 @@
+const addEventListener = () => {
+  const toggle = document.getElementById("toggleLight");
+
+  toggle.addEventListener("click", function () {
+    toggle.classList.toggle("bi-moon");
+    lightSwitchEffect();
+  });
+};
+
+const lightSwitchEffect = () => {
+  const toggle = document.getElementById("toggleLight");
+
+  const body = document.querySelector("body");
+  const h1 = document.querySelector("h1");
+  const p = document.querySelector("p");
+  const a = Array.from(document.querySelectorAll("a"));
+  const h2 = document.querySelector("h2");
+  const navbar = document.querySelector(".navbar");
+
+  if (toggle.classList.contains("bi-moon")) {
+    // Dark mode
+    body.style.background = "black";
+    body.style.color = "white";
+    body.style.transition = "2s";
+
+    h1.style.color = "white";
+    h1.style.transition = "2s";
+
+    const cards = document.querySelectorAll(".card");
+    const h2Elements = document.querySelectorAll("h2, a");
+
+    h2Elements.forEach((tag) => {
+      tag.style.color = "white";
+      tag.style.transition = "2s";
+    });
+
+    for (let i = 0; i < a.length; i = i + 1) {
+      a[i].style.color = "white";
+      a[i].style.transition = "2s";
+    }
+
+    cards.forEach((card) => {
+      card.style.background = "white";
+      card.style.transition = "2s";
+    });
+
+    navbar.style.borderBottom = "0.3px solid white";
+    navbar.style.transition = "2s";
+  } else {
+    // Dark mode
+
+    for (let i = 0; i < a.length; i = i + 1) {
+      a[i].style.color = "black";
+      a[i].style.transition = "2s";
+    }
+
+    const h2Elements = document.querySelectorAll("h2");
+
+    h2Elements.forEach((tag) => {
+      tag.style.color = "white";
+      tag.style.transition = "2s";
+    });
+
+    const cards = document.querySelectorAll(".card");
+    cards.forEach((card) => {
+      card.style.background = "black";
+      card.style.transition = "2s";
+    });
+
+    body.style.background = "white";
+    body.style.color = "black";
+    body.style.transition = "2s";
+    h1.style.color = "black";
+    h1.style.transition = "2s";
+    h2.style.color = "black";
+    h2.style.transition = "2s";
+
+    navbar.style.borderBottom = "0.3px solid black";
+    navbar.style.transition = "2s";
+  }
+};
+addEventListener();
